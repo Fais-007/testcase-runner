820ef8ea37304dce7d4a20f28a93b8c52b2fe255
Fais-007 <faisaliqbal669@gmail.com>
2020-07-11 11:46:13 +0530

    Fixes testcase file name to test.txt

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,50 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
-import { pathToFileURL } from "url";
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("terminalTest.sendText", () => {
-      if (ensureTerminalExists()) {
-        var ch = currentlyOpenTabfilePath?.split(".");
-        var currentlyOpenTabfilePath =
-          vscode.window.activeTextEditor?.document.uri.fsPath;
-        if (currentlyOpenTabfilePath) {
-          let ch = currentlyOpenTabfilePath.split(".");
-          if (fs.existsSync(ch[0] + ".txt")) {
-            vscode.workspace
-              .openTextDocument(ch[0] + ".txt")
-              .then((document) => {
-                let text = document.getText();
-                let terminal = vscode.window.activeTerminal;
-                if (terminal) {
-                  terminal.sendText(text);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("terminalTest.sendText", () => {
+            if (ensureTerminalExists()) {
+                var currentlyOpenTabfilePath =
+                    vscode.window.activeTextEditor?.document.uri.fsPath;
+                if (currentlyOpenTabfilePath) {
+                    var path = currentlyOpenTabfilePath.substring(
+                        0,
+                        currentlyOpenTabfilePath.lastIndexOf("\\") + 1
+                    );
+                    if (fs.existsSync(path + "test.txt")) {
+                        vscode.workspace
+                            .openTextDocument(path + "test.txt")
+                            .then((document) => {
+                                let text = document.getText();
+                                let terminal = vscode.window.activeTerminal;
+                                if (terminal) {
+                                    terminal.sendText(text);
+                                }
+                            });
+                    } else {
+                        vscode.window.showWarningMessage(
+                            "Test case file does not exists"
+                        );
+                    }
                 }
-              });
-          } else {
-            vscode.window.showWarningMessage("Test case file does not exists");
-          }
-        }
-      }
-    })
-  );
-
-  vscode.window.onDidCloseTerminal((terminal) => {
-    vscode.window.showInformationMessage(
-      `onDidCloseTerminal, name: ${terminal.name}`
+            }
+        })
     );
-  });
+
+    vscode.window.onDidCloseTerminal((terminal) => {
+        vscode.window.showInformationMessage(
+            `onDidCloseTerminal, name: ${terminal.name}`
+        );
+    });
 }
 
 function ensureTerminalExists(): boolean {
-  if ((<any>vscode.window).terminals.length === 0) {
-    vscode.window.showErrorMessage("No active terminals");
-    return false;
-  }
-  return true;
+    if ((<any>vscode.window).terminals.length === 0) {
+        vscode.window.showErrorMessage("No active terminals");
+        return false;
+    }
+    return true;
 }
 
 export function deactivate() {}
